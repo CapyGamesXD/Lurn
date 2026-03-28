46fd29aeffe4acb8213f8df7a551d57cd9395305
CapyGamesXD <luke.robert.steenkamp@gmail.com>
2026-03-28 21:25:03 +0000

    Added another meta property.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -174,6 +174,7 @@
 <!--This documentElement was courtesy of Claude. -->
 <svelte:head>
 	<title>LurnAI</title>
+	<meta property="og:description" content="An AI learning app!" />
 	<meta property="og:image" content="https://lurn.capydesigns.com/LurnAI.png" />
 	<script>
 		if (localStorage.getItem('dark') === 'true') {
