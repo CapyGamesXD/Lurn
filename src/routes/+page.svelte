7c788827079fab6bd8d1304bb4363fe15a762573
CapyGamesXD <luke.robert.steenkamp@gmail.com>
2026-03-28 21:24:32 +0000

    Fixed embed.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -174,7 +174,7 @@
 <!--This documentElement was courtesy of Claude. -->
 <svelte:head>
 	<title>LurnAI</title>
-	<meta property="og:image" content="LurnAI.png" />
+	<meta property="og:image" content="https://lurn.capydesigns.com/LurnAI.png" />
 	<script>
 		if (localStorage.getItem('dark') === 'true') {
 			document.documentElement.classList.toggle('dark');
